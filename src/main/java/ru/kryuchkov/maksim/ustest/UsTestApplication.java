908fb5816b17775c9cd16656bd7e84aea8f12177
Maxim Kryuchkov <7293121+observant-sun@users.noreply.github.com>
2025-11-13 14:16:15 +0300

    Initial commit

---
+++ src/main/java/ru/kryuchkov/maksim/ustest/UsTestApplication.java
@@ -0,0 +1,13 @@
+package ru.kryuchkov.maksim.ustest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kryuchkov/maksim/ustest/UsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kryuchkov.maksim.ustest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
